08a881167de2f53a1a90411718a6876198f6d0a6
irack000 <IRACK000@gmail.com>
2022-05-31 17:10:13 +0900

    add basic files

---
+++ src/test/test_client.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+### Alias : test_client & Last Modded : 2022.05.31. ###
+Coded with Python 3.10 Grammar by irack000
+Description : test client
+Reference : ?
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
